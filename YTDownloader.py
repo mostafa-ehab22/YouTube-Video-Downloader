bcfac85c448c0c62e885acf53a9fff14e5b12112
Mostafa Ehab <mostafaehab0002@gmail.com>
2024-06-25 16:25:39 +0300

    Added user confirmation

--- YTDownloader.py
+++ YTDownloader.py
@@ -11,13 +11,15 @@ yt = YouTube(link)
 print("Title:",yt.title)
 print("Views:","{:,}".format(yt.views))
 
+user_input = input("Do you want to download video? (y/n) ").strip().lower()
 
-# Getting Video Resolution
-resolution = yt.streams.filter(res="480p",progressive=True).first()
+if user_input == 'y':
+    # Getting Video Resolution
+    resolution = yt.streams.filter(res="480p",progressive=True).first()
 
-# Checking if wanted resolution available
-if resolution:
-    resolution.download("D:\Mostafa\YT Videos")
-    print("Download Successfull!")
-else:
-    print("Video Resolution not availabe")
+    # Checking if wanted resolution available
+    if resolution:
+        resolution.download("D:\Mostafa\YT Videos")
+        print("Download Successfull!")
+    else:
+        print("Video Resolution not availabe")
