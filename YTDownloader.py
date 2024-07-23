b0d5050cf51ed5589825b038a52e295f550bffb9
Mostafa Ehab <mostafaehab0002@gmail.com>
2024-07-23 20:15:09 +0300

    Improved erro handling

--- YTDownloader.py
+++ YTDownloader.py
@@ -1,25 +1,41 @@
 from pytube import YouTube
-from sys import argv
+import sys
 """
 Running program: python YTDownloader.py "LINK"
 """
 
-# Takes 1st line after program name & stores it as link
-link = argv[1]
-yt = YouTube(link)
+def main():
+    
+    if len(sys.argv) != 2:
+        sys.exit("Usage: python YTDownloader.py <LINK>")
+    
+    # Fetch video Link
+    link = sys.argv[1]
+    try:
+        yt = YouTube(link)
+    except Exception as e:
+        sys.exit(f"Failed to fetch video: {e}")
 
-print(f"Title: {yt.title}")
-print(f"Views: {yt.views:,}")
+    print(f"Title: {yt.title}")
+    print(f"Views: {yt.views:,}")
 
-user_input = input("Do you want to download video? (y/n) ").strip().lower()
+    confirmation = input("Do you want to download video? (y/n): ").strip().lower()
 
-if user_input == 'y':
-    # Getting Video Resolution
-    resolution = yt.streams.filter(res="480p",progressive=True).first()
+    if confirmation == 'y':
+        # Getting Video Resolution
+        resolution_input = input("Choose resolution (e.g., 720): ").strip()
+        resolution = yt.streams.filter(res=f"{resolution_input}p",progressive=True).first()
 
-    # Checking if wanted resolution available
-    if resolution:
-        resolution.download("D:\Mostafa\YT Videos")
-        print("Download Successful!")
-    else:
-        print("Video Resolution not available")
+        try:
+            # Checking if wanted resolution available
+            if resolution:
+                resolution.download("D:\Mostafa\YT Videos")
+                print("Download Successful!")
+            else:
+                print("Video Resolution not available")
+        except Exception as e:
+            print(f"Couldn't download video: {e}")
+
+
+if __name__ == "__main__":
+    main()
